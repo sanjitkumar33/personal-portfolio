b52040c515b2fe96ee48b0e05474f979c237f53b
sanjitkumar33 <kumarsan5233@gmail.com>
2025-08-18 11:37:00 +0530

    first commit

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,12 +1,89 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const About = () => {
+    const frontEndSkills = [ "React", "Vue", "TypeScript", "TailwindCss", "svelte",];
+    const BackEndSkills = [ "Node.js", "Python", "MongoDB", "Express.js", "PostGre",];
     return (
         <section id="about" className="min-h-screen flex items-center juustify-center py-20">
-            <div className="max--w-3xl mx-auto px-4">
+           <RevealOnScroll>
+            <div className="max-w-3xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+                    {" "}
                     About Me
                 </h2>
-            </div>
+                <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+                    <p className="text-gray-300 mb-6">
+                        Passionate developer with expertise in building scalable web application 
+                        and creative innovative solutions.
+                    </p>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">
+                               FrontEnd Developer 
+                            </h3>
+                            <div className="flex flex-wrap gap-2">
+                                {frontEndSkills.map((tech, key) => (
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">
+                               Backend Developer 
+                            </h3>
+                            <div className="flex flex-wrap gap-2">
+                                {BackEndSkills.map((tech, key) => (
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4"> Education </h3>
+                        <ul className="list-disc list-inside text-gray-300 space-y-2">
+                            <li>
+                                <strong>
+                                    B.E in Electronics & Communication Engineering 
+                                </strong> in RTMNU (2012-2016)
+                            </li>
+                            <li>Relevant Course Full Stack Developer </li>
+                        </ul>
 
-        </section>
-    )
-}
+                    </div>
+                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                    <h3 className="text-xl font-bold mb-4"> Work Experience </h3>
+                    <div className="space-y-4 text-gray-300">
+                        <div>
+                            <h4 className="font-semibold">
+                                NME digital agency (2018-2020)
+                            </h4>
+                            <p>
+                                website designer 
+                            </p>
+                        </div>
+                        <div>
+                            <h4 className="font-semibold">
+                                Edureka Training & Working (2022-2023)
+                            </h4>
+                            <p>
+                             Full Stack Developer 
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+</RevealOnScroll>
+    </section>
+    );
+};
